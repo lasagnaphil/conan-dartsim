31fedac0cd78b2421cb418938e5fd172821dbe09
Devin Schwab <digidevin@gmail.com>
2019-07-24 22:26:10 -0700

    :white_check_mark: Test package simulating multi-pendulum

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,7 +1,129 @@
+#include <dart/dart.hpp>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Bincrafters\n";
-    return 0;
+const double default_height = 1.0; // m
+const double default_width = 0.2;  // m
+const double default_depth = 0.2;  // m
+
+const double default_torque = 15.0; // N-m
+const double default_force = 15.0;  // N
+const int default_countdown = 200;  // Number of timesteps for applying force
+
+const double default_rest_position = 0.0;
+const double delta_rest_position = 10.0 * M_PI / 180.0;
+
+const double default_stiffness = 0.0;
+const double delta_stiffness = 10;
+
+const double default_damping = 5.0;
+const double delta_damping = 1.0;
+
+using namespace dart::dynamics;
+using namespace dart::simulation;
+
+void setGeometry(const BodyNodePtr &bn) {
+  // Create a BoxShape to be used for both visualization and collision checking
+  std::shared_ptr<BoxShape> box(new BoxShape(
+      Eigen::Vector3d(default_width, default_depth, default_height)));
+
+  // Create a shape node for visualization and collision checking
+  auto shapeNode =
+      bn->createShapeNodeWith<VisualAspect, CollisionAspect, DynamicsAspect>(
+          box);
+  shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
+
+  // Set the location of the shape node
+  Eigen::Isometry3d box_tf(Eigen::Isometry3d::Identity());
+  Eigen::Vector3d center = Eigen::Vector3d(0, 0, default_height / 2.0);
+  box_tf.translation() = center;
+  shapeNode->setRelativeTransform(box_tf);
+
+  // Move the center of mass to the center of the object
+  bn->setLocalCOM(center);
+}
+
+BodyNode *makeRootBody(const SkeletonPtr &pendulum, const std::string &name) {
+  BallJoint::Properties properties;
+  properties.mName = name + "_joint";
+  properties.mRestPositions = Eigen::Vector3d::Constant(default_rest_position);
+  properties.mSpringStiffnesses = Eigen::Vector3d::Constant(default_stiffness);
+  properties.mDampingCoefficients = Eigen::Vector3d::Constant(default_damping);
+
+  auto bodyProp = BodyNode::Properties(BodyNode::AspectProperties(name));
+  BodyNodePtr bn =
+      pendulum
+          ->createJointAndBodyNodePair<BallJoint>(nullptr, properties, bodyProp)
+          .second;
+
+  // Make a shape for the Joint
+  const double &R = default_width;
+  std::shared_ptr<EllipsoidShape> ball(
+      new EllipsoidShape(sqrt(2) * Eigen::Vector3d(R, R, R)));
+  auto shapeNode = bn->createShapeNodeWith<VisualAspect>(ball);
+  shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
+
+  // Set the geometry of the Body
+  setGeometry(bn);
+
+  return bn;
+}
+
+BodyNode *addBody(const SkeletonPtr &pendulum, BodyNode *parent,
+                  const std::string &name) {
+  // Set up the properties for the Joint
+  RevoluteJoint::Properties properties;
+  properties.mName = name + "_joint";
+  properties.mAxis = Eigen::Vector3d::UnitY();
+  properties.mT_ParentBodyToJoint.translation() =
+      Eigen::Vector3d(0, 0, default_height);
+  properties.mRestPositions[0] = default_rest_position;
+  properties.mSpringStiffnesses[0] = default_stiffness;
+  properties.mDampingCoefficients[0] = default_damping;
+
+  // Create a new BodyNode, attached to its parent by a RevoluteJoint
+  BodyNodePtr bn = pendulum
+                       ->createJointAndBodyNodePair<RevoluteJoint>(
+                           parent, properties, BodyNode::AspectProperties(name))
+                       .second;
+
+  // Make a shape for the Joint
+  const double R = default_width / 2.0;
+  const double h = default_depth;
+  std::shared_ptr<CylinderShape> cyl(new CylinderShape(R, h));
+
+  // Line up the cylinder with the Joint axis
+  Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+  tf.linear() =
+      dart::math::eulerXYZToMatrix(Eigen::Vector3d(90.0 * M_PI / 180.0, 0, 0));
+
+  auto shapeNode = bn->createShapeNodeWith<VisualAspect>(cyl);
+  shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
+  shapeNode->setRelativeTransform(tf);
+
+  // Set the geometry of the Body
+  setGeometry(bn);
+
+  return bn;
+}
+
+int main() {
+  // Create an empty Skeleton with the name "pendulum"
+  SkeletonPtr pendulum = Skeleton::create("pendulum");
+
+  // Add each body to the last BodyNode in the pendulum
+  BodyNode *bn = makeRootBody(pendulum, "body1");
+  bn = addBody(pendulum, bn, "body2");
+  bn = addBody(pendulum, bn, "body3");
+  bn = addBody(pendulum, bn, "body4");
+  bn = addBody(pendulum, bn, "body5");
+
+  // Set the initial position of the first DegreeOfFreedom so that the pendulum
+  // starts to swing right away
+  pendulum->getDof(1)->setPosition(120 * M_PI / 180.0);
+
+  // Create a world and add the pendulum to the world
+  WorldPtr world = World::create();
+  world->addSkeleton(pendulum);
+
+  world->step();
 }
