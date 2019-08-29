65efca01fa9f5158b9a26f4adf261a2a60f3d194
Devin Schwab <digidevin@gmail.com>
2019-08-29 12:16:22 -0700

    :sparkles: Updated build script to build python 3.7 bindings

--- build.py
+++ build.py
@@ -21,8 +21,13 @@ if __name__ == "__main__":
         new_settings = copy.copy(settings)
         new_settings["compiler.cppstd"] = "17"
         new_settings["compiler.libcxx"] = "libstdc++11"
+        new_options = copy.copy(options)
+        new_options["dart:build_dartpy"] = True
+        new_options["dart:python_version"] = "3.7"
         new_builds.append([settings, options, env_vars, build_requires])
         new_builds.append([new_settings, options, env_vars, build_requires])
+        new_builds.append([settings, new_options, env_vars, build_requires])
+        new_builds.append([new_settings, new_options, env_vars, build_requires])
     builder.builds = new_builds
 
     builder.run()
